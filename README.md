21e3b846c5d5e53069455823c51f4f45d09ba76e
Monu <kdie44@gmail.com>
2021-11-07 20:32:43 +0900

    22시 30분 최종 완료(오류 수정 완료, 리액트 다운그레이드로 오류 해결)

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ React JS Fundamentals Course, this repository created on Nov 07, 2021
 ## async
 자바스크립트에게 해당 함수가 비동기임을 알려준다.
 async와 await는 함께 쓰인다.
+
+## react 6 다운그레이드
+`routes`사용 고정으로 오류가 났다->다운그레이드 하는 것으로 해결
+```
+npm uninstall
+npm install react-router-dom@5
+```
+## 완료
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -8,14 +8,12 @@ import "./App.css";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Navigation />
-            <Routes>
-                <Route path="/" exact={true} component={<Home/>} />
-                <Route path="/about" component={<About/>} />
-                <Route path="/movie/:id" component={<Detail/>} />
-            </Routes>
-        </BrowserRouter>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/movie/:id" component={Detail} />
+        </HashRouter>
     );
 }
 
